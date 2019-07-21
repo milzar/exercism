776cce7bd43e27da4f38ba725fe24134af3ce1d0
milzar <realmilanxavier.com>
2019-07-21 14:32:28 +0530

    Refactored code

--- twelve-days/src/main/java/TwelveDays.java
+++ twelve-days/src/main/java/TwelveDays.java
@@ -1,58 +1,42 @@
 import java.util.HashMap;
 
 class TwelveDays {
-    HashMap<Integer,String> dayMap;
-    HashMap<Integer, String> verseMap;
-
+    HashMap<Integer,String> dayInWords;
+    HashMap<Integer, String> gifts;
 
     TwelveDays(){
-        dayMap = new HashMap<Integer,String>();
-        verseMap= new HashMap<Integer, String>();
-        dayMap.put(1,"first");
-        dayMap.put(2,"second");
-        dayMap.put(3,"third");
-        dayMap.put(4,"fourth");
-        dayMap.put(5,"fifth");
-        dayMap.put(6,"sixth");
-        dayMap.put(7,"seventh");
-        dayMap.put(8,"eighth");
-        dayMap.put(9,"ninth");
-        dayMap.put(10,"tenth");
-        dayMap.put(11,"eleventh");
-        dayMap.put(12,"twelfth");
+        dayInWords= new HashMap<Integer,String>();
+        gifts= new HashMap<Integer, String>();
+        dayInWords.put(1,"first");
+        dayInWords.put(2,"second");
+        dayInWords.put(3,"third");
+        dayInWords.put(4,"fourth");
+        dayInWords.put(5,"fifth");
+        dayInWords.put(6,"sixth");
+        dayInWords.put(7,"seventh");
+        dayInWords.put(8,"eighth");
+        dayInWords.put(9,"ninth");
+        dayInWords.put(10,"tenth");
+        dayInWords.put(11,"eleventh");
+        dayInWords.put(12,"twelfth");
 
-        verseMap.put(1,"a Partridge in a Pear Tree");
-		verseMap.put(2,"two Turtle Doves");
-		verseMap.put(3,"three French Hens");
-		verseMap.put(4,"four Calling Birds");
-		verseMap.put(5,"five Gold Rings");
-		verseMap.put(6,"six Geese-a-Laying");
-		verseMap.put(7,"seven Swans-a-Swimming");
-		verseMap.put(8,"eight Maids-a-Milking");
-		verseMap.put(9,"nine Ladies Dancing");
-		verseMap.put(10,"ten Lords-a-Leaping");
-		verseMap.put(11,"eleven Pipers Piping");
-        verseMap.put(12,"twelve Drummers Drumming");
-        
-
-    }
-
-    String getVerseEnd(int verseNumber){
-        StringBuilder verseEnd = new StringBuilder();
-
-        for(int i=verseNumber;i>1;i--){
-            verseEnd.append(  String.format("%s, ", verseMap.get(i) )  );
-            if(i == 2)
-                verseEnd.append( String.format("and "));
-        }
-
-        verseEnd.append(verseMap.get(1));
-        
-        return verseEnd.toString();
+        gifts.put(1,"a Partridge in a Pear Tree");
+		gifts.put(2,"two Turtle Doves");
+		gifts.put(3,"three French Hens");
+		gifts.put(4,"four Calling Birds");
+		gifts.put(5,"five Gold Rings");
+		gifts.put(6,"six Geese-a-Laying");
+		gifts.put(7,"seven Swans-a-Swimming");
+		gifts.put(8,"eight Maids-a-Milking");
+		gifts.put(9,"nine Ladies Dancing");
+		gifts.put(10,"ten Lords-a-Leaping");
+		gifts.put(11,"eleven Pipers Piping");
+        gifts.put(12,"twelve Drummers Drumming");
     }
 
     String verse(int verseNumber) {
-        return String.format("On the %s day of Christmas my true love gave to me: %s.\n",dayMap.get(verseNumber), getVerseEnd(verseNumber));
+        return String.format("On the %s day of Christmas my true love gave to me: %s.\n",
+                            dayInWords.get(verseNumber), getVerseEndinging(verseNumber));
     }
 
     String verses(int startVerse, int endVerse) {
@@ -67,5 +51,18 @@ class TwelveDays {
 
     String sing() {
           return verses(1,12);
+    }
+
+    String getVerseEndinging(int verseNumber){
+        StringBuilder verseEndBuilder = new StringBuilder();
+
+        for(int i=verseNumber;i>1;i--){
+            verseEndBuilder.append(  String.format("%s, ", gifts.get(i) ) );
+            if(i == 2)
+                verseEndBuilder.append( String.format("and "));
+        }
+
+        verseEndBuilder.append(gifts.get(1));
+        return verseEndBuilder.toString();
     }
 }
